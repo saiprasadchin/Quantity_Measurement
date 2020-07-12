87349d59c01da54160994594643beee17b8c8fe4
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-12 08:17:21 -0700

    Added : Test case for Feet not equal

--- src/test/java/com/measurement/QuantityMeasurementTest.java
+++ src/test/java/com/measurement/QuantityMeasurementTest.java
@@ -12,4 +12,12 @@ public class QuantityMeasurementTest {
         double inch2 = quantityMeasurement.returnUnit(12.0, 0.0);
         Assert.assertEquals(inch1,inch2,0.0);
     }
+
+    @Test
+    public void given0FeetAnd1Feet_IfNotEqual_ShouldReturnTrue() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double inch1 = quantityMeasurement.returnUnit(12.0, 0.0);
+        double inch2 = quantityMeasurement.returnUnit(12.0, 1.0);
+        Assert.assertNotEquals(inch1,inch2,0.0);
+    }
 }
