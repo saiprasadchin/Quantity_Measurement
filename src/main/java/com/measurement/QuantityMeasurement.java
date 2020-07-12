496819a5fc410784f9fb6bae7746733d1fbde4e0
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-12 07:13:27 -0700

    Welcome message

---
+++ src/main/java/com/measurement/QuantityMeasurement.java
@@ -0,0 +1,7 @@
+package com.measurement;
+
+public class QuantityMeasurement {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quantity Measurement problem");
+    }
+}
