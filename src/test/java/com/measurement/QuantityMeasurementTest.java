186dec09c16161c937d900e3ba640e978d9b71ee
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-15 11:54:16 -0700

    Added : Test case to check 1 liter equal to 1000 milliliter

--- src/test/java/com/measurement/QuantityMeasurementTest.java
+++ src/test/java/com/measurement/QuantityMeasurementTest.java
@@ -305,4 +305,11 @@ public class QuantityMeasurementTest {
         litre2 = quantityMeasurement.returnUnit(UnitType.LITRE, 3.785);
         Assert.assertEquals(litre1, litre2, 0.0);
     }
+
+    @Test
+    public void given1LitreAnd1000Millilitre_WhenEqual_ShouldReturnTrue() {
+        litre1 = quantityMeasurement.returnUnit(UnitType.LITRE, 1.0);
+        litre2 = quantityMeasurement.returnUnit(UnitType.MILLILITRE, 1000.0);
+        Assert.assertEquals(litre1, litre2, 0.0);
+    }
 }
